32eccb66b7c47dec1d7ad714fe44b72b59d0dfe2
wearelish <wearelish1029@gmail.com>
2026-04-26 14:50:56 +0530

    switch to Mumbai Supabase for faster performance

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL ?? "https://tkntnxiqeefhtogpblwf.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrbnRueGlxZWVmaHRvZ3BibHdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNzQ0NDgsImV4cCI6MjA5MjY1MDQ0OH0.zkKeIkmtUBVKhnUG0yhWF9r01gnJxrXIeZOvr_AZvDE";
+const SUPABASE_URL = "https://yevdyhdifwwkbiulrzmk.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlldmR5aGRpZnd3a2JpdWxyem1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxODM0MzUsImV4cCI6MjA5Mjc1OTQzNX0.1vEBNzZFCc24oISfqY3tdTmJCiB11klEqHPc-wfkspQ";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
